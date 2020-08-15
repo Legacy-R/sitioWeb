1c855657e9e93cba3aee94e27e70ed780bc99203
Roberto Pajares <65995247+Legacy-R@users.noreply.github.com>
2020-08-15 06:25:32 -0500

    validacion de formulario y ajuste de pantalla media mobile

---
+++ js/validacion.js
@@ -0,0 +1,25 @@
+var rules = {
+    nombre: {required:true,minlength: 3},
+    correo: {required:true,email:true},
+    telefono: {required:true,minlength: 9, maxlength:9},
+    mensaje:{required: true},
+};
+let mensajes = {
+    nombre: {required:"Nombre Requerido"},
+    correo: {required:"ingresar correo electrónico correctamente", email:"Formato de Correo electrónico incorrecto"},
+    telefono:{required:"ingresar un número de celular valido", minlength:"Caracteres minimos 9", maxlength:"Caracteres permitidos 9"},
+    mensaje:{required:"Escribe un mensaje"},
+};
+
+$(document).ready (function(){
+    $("#idFormulario").submit(function(e) {
+        e.preventDefault();
+    }).validate({
+        rules:rules,
+        messages:mensajes,
+        submitHandler: function(form) {
+            $('.alert-success').show(1000);
+            return false;
+        }
+    });
+});
